1a9042691965d0d7025914e8acd152c4994ae74d
Andreas Umbach <umbach@gmail.com>
2017-04-13 09:08:40 +0200

    added MouseListener to Pause/Resume the animation with a click

--- src/AnimatedSquare.java
+++ src/AnimatedSquare.java
@@ -1,18 +1,23 @@
 import java.awt.Dimension;
 import java.awt.Graphics;
+import java.awt.event.MouseEvent;
+import java.awt.event.MouseListener;
 
 import javax.swing.JFrame;
 import javax.swing.JPanel;
 
-public class AnimatedSquare extends JPanel implements Runnable {
+public class AnimatedSquare extends JPanel implements Runnable, MouseListener {
 
 	double t = 0;
+	boolean isPaused = false;
 
+	
 	public static void main(String[] args) {
-		JFrame frame = new JFrame();
 		AnimatedSquare sq = new AnimatedSquare();
 		sq.setPreferredSize(new Dimension(400, 400));
-
+		sq.addMouseListener(sq);
+		
+		JFrame frame = new JFrame();
 		frame.add(sq);
 		frame.pack();
 		frame.setVisible(true);
@@ -24,6 +29,7 @@ public class AnimatedSquare extends JPanel implements Runnable {
 	@Override
 	public void paint (Graphics g)
 	{
+		super.paint(g);
 		int w = this.getWidth();
 		int h = this.getHeight();
 		
@@ -31,22 +37,52 @@ public class AnimatedSquare extends JPanel implements Runnable {
 		g.drawLine( w, (int) (t * h), (int) ( (1-t) * w), h );
 		g.drawLine( (int) ( (1-t) * w), h, 0, (int) ( (1-t) * h) );
 		g.drawLine( 0, (int) ( (1-t) * h), (int) (t * w), 0 );
-		
 	}
 
 	@Override
 	public void run() {
 		while(true)
 		{
-			t += 0.01;
-			if(t > 1)
-				t -= 1;
-//			System.out.println(t);
-			repaint();
+			if(!isPaused)
+			{
+				t += 0.01;
+				if(t > 1)
+					t -= 1;
+				repaint();
+			}
 			try { 
 				Thread.sleep(10);
 			}
 			catch (Exception e) { /* ignore exception */ }
 		}
+	}
+
+	@Override
+	public void mouseClicked(MouseEvent e) {
+		this.isPaused = !this.isPaused;
+	}
+
+	@Override
+	public void mousePressed(MouseEvent e) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void mouseReleased(MouseEvent e) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void mouseEntered(MouseEvent e) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void mouseExited(MouseEvent e) {
+		// TODO Auto-generated method stub
+		
 	}
 }
